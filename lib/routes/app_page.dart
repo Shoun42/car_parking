ae395a8ff449697d41e2c17588c0497dd3e52fc3
User <shounbabu31@gmail.com>
2025-11-24 03:20:08 +0600

    update

--- lib/routes/app_page.dart
+++ lib/routes/app_page.dart
@@ -1,6 +1,10 @@
 import 'package:car_parking/routes/app_routes.dart';
+import 'package:get/get.dart';
 
 class AppRoute{
 static final initialPage=AppPage.home;
 
+static final routes=[
+ // GetPage(name: AppPage.home, page: ()=>);
+];
 }

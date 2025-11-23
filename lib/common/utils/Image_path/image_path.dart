1c15dcad65b0903b2e765a4d42444dcd282be7dd
User <shounbabu31@gmail.com>
2025-11-24 03:13:26 +0600

    update

---
+++ lib/common/utils/Image_path/image_path.dart
@@ -0,0 +1,7 @@
+class ImagePath{
+  static final String _baseUrlPath="assets/images";
+  static final String splashlogo= "$_baseUrlPath/logo.png";
+  static final String onboading_One= "$_baseUrlPath/onboading1.png";
+  static final String onboading_Two= "$_baseUrlPath/onboading2.png";
+  static final String onboading_Three= "$_baseUrlPath/onboading3.png";
+}

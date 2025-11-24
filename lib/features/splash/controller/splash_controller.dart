afda6a068903345ac2b961d01ea5c430de0e3e80
User <shounbabu31@gmail.com>
2025-11-25 05:59:47 +0600

    update

--- lib/features/splash/controller/splash_controller.dart
+++ lib/features/splash/controller/splash_controller.dart
@@ -1,5 +1,18 @@
+
+
+import 'package:car_parking/routes/app_routes.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
+Future <void> splashToOnboadingScreen () async{
+  await Future.delayed(Duration(seconds: 2));
+  Get.offNamed(AppPage.onBoadigOnePage);
+}
+@override
+  void onReady() {
+    // TODO: implement dispose
+    super.onReady();
+    splashToOnboadingScreen();
+  }
 
 }

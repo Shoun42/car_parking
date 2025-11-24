0c7b5573a8bf5453af19051c2e21239c338091f8
User <shounbabu31@gmail.com>
2025-11-25 05:58:19 +0600

    update

--- lib/routes/app_page.dart
+++ lib/routes/app_page.dart
@@ -1,10 +1,13 @@
+import 'package:car_parking/features/splash/view/splash_view.dart';
 import 'package:car_parking/routes/app_routes.dart';
 import 'package:get/get.dart';
 
-class AppRoute{
+import '../features/splash/binding/splash_bindings.dart';
+
+class AppRoutes{
 static final initialPage=AppPage.home;
 
 static final routes=[
- // GetPage(name: AppPage.home, page: ()=>);
+  GetPage(name: AppPage.home, page: ()=>SplashView(),binding:SplashBinding()),
 ];
 }

6819719ccac3d2b1e9a746755a1fb53c45f9a5a4
User <shounbabu31@gmail.com>
2025-11-25 06:00:04 +0600

    update

--- lib/features/splash/view/splash_view.dart
+++ lib/features/splash/view/splash_view.dart
@@ -1,9 +1,11 @@
 import 'package:car_parking/common/utils/Image_path/image_batton_icon/image_batton_icon.dart';
 import 'package:car_parking/common/utils/Image_path/image_path.dart';
 import 'package:car_parking/common/utils/app_color/app_color.dart';
+import 'package:car_parking/features/splash/controller/splash_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends GetView <SplashController> {
   const SplashView({super.key});
 
   @override
@@ -15,8 +17,6 @@ class SplashView extends StatelessWidget {
         child:
         Image.asset(ImagePath.splashlogo),
       ),
-
-
     );
   }
 }

c31dd35dda57fc7a10790ba469ae1da513540320
User <shounbabu31@gmail.com>
2025-11-28 22:28:26 +0600

    update

---
+++ lib/features/onboading/binding/onboading_bindings.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+import '../contollar/oboading_controller.dart';
+
+class OnboadingBinding extends Bindings{
+
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+    Get.put(OboadingOneController);
+  }}

58a4b22be5c52adc71a3dc71c681bf21ca7f9ad2
User <shounbabu31@gmail.com>
2025-11-30 03:38:04 +0600

    update

---
+++ lib/features/onboading/view/onboading_two_screen.dart
@@ -0,0 +1,18 @@
+import 'package:car_parking/common/utils/app_color/app_color.dart';
+import 'package:flutter/material.dart';
+
+class OnboadingTwoScreen extends StatelessWidget {
+  const OnboadingTwoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.surfacePrimary,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey),
+      ),
+
+
+    );
+  }
+}

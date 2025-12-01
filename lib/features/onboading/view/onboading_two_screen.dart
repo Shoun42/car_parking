46bfa1fe0655e18b6fe685ee7f601ab081bcb153
User <shounbabu31@gmail.com>
2025-12-01 09:49:36 +0600

    update

--- lib/features/onboading/view/onboading_two_screen.dart
+++ lib/features/onboading/view/onboading_two_screen.dart
@@ -1,17 +1,46 @@
 import 'package:car_parking/common/utils/app_color/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-class OnboadingTwoScreen extends StatelessWidget {
+import '../contollar/oboading_controller.dart';
+
+class OnboadingTwoScreen extends GetView<OnboadingOneController> {
   const OnboadingTwoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final theme =Theme.of(context).textTheme;
     return Scaffold(
       backgroundColor: AppColor.surfacePrimary,
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.grey),
-      ),
+        backgroundColor: AppColor.surfacePrimary,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              
+            ),Text("Skip",style: theme.labelSmall,)
+          ],
+        ),
 
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SizedBox(height: 291.h,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+
+              ],
+            )
+          ],
+        ),
+
+      ),
 
     );
   }

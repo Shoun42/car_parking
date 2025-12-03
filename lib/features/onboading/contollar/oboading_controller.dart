56bc91a1b5e307863168d51b47561d0ae28779c0
User <shounbabu31@gmail.com>
2025-12-03 17:38:07 +0600

    update

--- lib/features/onboading/contollar/oboading_controller.dart
+++ lib/features/onboading/contollar/oboading_controller.dart
@@ -2,13 +2,17 @@ import 'package:car_parking/routes/app_routes.dart';
 import 'package:get/get.dart';
 
 
-class OboadingOneController extends GetxController {
+class OnboadingOneController extends GetxController {
   void goToAnotherOnboadingScreen(){
     Get.toNamed(AppPage.onBoadigTwoPage );
 
   }
   void secondOnboadingToNext(){
-    Get.toNamed(AppPage.onBoadigOnePage);
+    Get.toNamed(AppPage.onBoadingThreePage);
+  }
+
+  void onboadingToLogInScreen(){
+    Get.toNamed(AppPage.sign_in);
   }
 
 }

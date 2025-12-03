57d6405986acfe872dbd846fc9cee749c6f80815
User <shounbabu31@gmail.com>
2025-12-03 16:24:19 +0600

    update

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,4 +2,8 @@ class AppPage {
   static final String home = "/home";
   static final String onBoadigOnePage="/onboading_one";
   static final String onBoadigTwoPage="/onboading_two";
+  static final String onBoadingThreePage = '/onboading_three';
+  static final String sign_in = '/sign_in';
+  static final String sign_in_two ='/sign_in_two';
+
 }

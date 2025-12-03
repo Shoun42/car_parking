243668ef56a624a3e40a17d56b57988baf711a42
User <shounbabu31@gmail.com>
2025-12-03 09:13:00 +0600

    update

--- lib/routes/app_page.dart
+++ lib/routes/app_page.dart
@@ -1,16 +1,29 @@
+
+
+import 'package:car_parking/features/authentication/sign_in/bindings/sign_in_bindings.dart';
+import 'package:car_parking/features/authentication/sign_in/view/sign_in_two.dart';
+import 'package:car_parking/features/authentication/sign_in/view/view.dart';
 import 'package:car_parking/features/onboading/binding/onboading_bindings.dart';
 import 'package:car_parking/features/onboading/view/onboading_one_screen.dart';
+import 'package:car_parking/features/onboading/view/onboading_three_screen.dart';
 import 'package:car_parking/features/splash/view/splash_view.dart';
 import 'package:car_parking/routes/app_routes.dart';
 import 'package:get/get.dart';
 
+import '../features/onboading/view/onboading_two_screen.dart';
 import '../features/splash/binding/splash_bindings.dart';
 
 class AppRoutes{
 static final initialPage=AppPage.home;
 
 static final routes=[
+
   GetPage(name: AppPage.home, page: ()=>SplashView(),binding:SplashBinding()),
   GetPage(name: AppPage.onBoadigOnePage, page: ()=>OnboadingOneScreen(),binding:OnboadingBinding()),
+  GetPage(name: AppPage.onBoadigTwoPage, page: ()=>OnboadingTwoScreen(),binding: OnboadingBinding()),
+  GetPage(name: AppPage.onBoadingThreePage, page: ()=>OnboadingThreeScreen(),binding: OnboadingBinding()),
+  GetPage(name: AppPage.sign_in, page: ()=>SignInView(),binding: SignInBindins()),
+  GetPage(name: AppPage.sign_in_two, page: ()=>SignInTwo(),binding: SignInBindins())
+
 ];
 }

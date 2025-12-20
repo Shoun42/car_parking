79dccda7ea1b6a9e3955d6fe0ae6dc442a9d4b22
User <shounbabu31@gmail.com>
2025-12-20 10:55:51 +0600

    update

---
+++ lib/features/authentication/sign_in/controller/forget_controller.dart
@@ -0,0 +1,13 @@
+import 'package:car_parking/routes/app_routes.dart';
+import 'package:get/get.dart';
+
+class ForgetController extends GetxController{
+
+  void ForgetPassword (){
+    Get.toNamed(AppPage.otp_screeen);
+
+  }
+
+
+  
+}

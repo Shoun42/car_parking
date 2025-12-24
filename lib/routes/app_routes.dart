d9387ec3ca844417366546ad9debe73643764a2e
User <shounbabu31@gmail.com>
2025-12-24 21:01:10 +0600

    update

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -6,6 +6,8 @@ class AppPage {
   static final String sign_in = '/sign_in';
   static final String otp_screeen ='/OtpScreen';
   static final String forget_password ='/ForgetPassword';
+  static final String reset_password ='/ResetPassword';
+  static final String sign_up_screen ='/SignUpScreen';
 
 
 }
